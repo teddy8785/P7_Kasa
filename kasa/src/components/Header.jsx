1fde4c21106f82d15f92c45138af6cf833613f1f
teddy8587 <teddy.barieraud@gmail.com>
2024-07-01 07:24:57 +0200

    first commit

---
+++ kasa/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div>
+      <h1>Kasa</h1>
+      <nav>
+        <a href="#">Accueil</a>
+        <a href="#">A Propos</a>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
