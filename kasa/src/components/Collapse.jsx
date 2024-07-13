090608a57fd048276a713dd779cc4b72e3681cfb
teddy8587 <teddy.barieraud@gmail.com>
2024-07-13 09:45:51 +0200

    perf animation des chevrons page Apropos

--- kasa/src/components/Collapse.jsx
+++ kasa/src/components/Collapse.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import "../styles/Collapse.css";
 import chevronUp from "../assets/chevron-up-solid.svg";
-import chevronDown from "../assets/chevron-down-solid.svg";
 
 function Collapse() {
 
@@ -18,32 +17,41 @@ function Collapse() {
         'La bienveillance fait partie des valeurs fondatrice de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
         'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'
     ];
-   
-  const [commentOff, commentOn] = useState(null);
 
-  const toggleSection = (index) => {
-    commentOn(commentOff === index ? null : index);
-  };
+    const [expandedSections, setExpandedSections] = useState([]);
 
-  return (
-    <div className="collapse__content">
-      {titleCollapse.map((title, index) => (
-        <div key={index} className="collapse__comment--content">
-          <div className="collapse">
-            <p className="collapse__title">{title}</p>
-            <img
-              className="collapse__chevron" onClick={() => toggleSection(index)}
-              src={commentOff === index ? chevronDown : chevronUp}
-              alt="Chevron"
-            />
-          </div>
-          {commentOff === index && (
-              <p className="collapse__comment">{commentCollapse[index]}</p>
-          )}
+    const toggleSection = (index) => {
+        if (expandedSections.includes(index)) {
+            // Si l'index est déjà ouvert, le retire de la liste pour le fermer
+            setExpandedSections(expandedSections.filter(secIndex => secIndex !== index));
+        } else {
+            // Sinon, l'ajoute à la liste pour l'ouvrir
+            setExpandedSections([...expandedSections, index]);
+        }
+    };
+
+    return (
+        <div className="collapse__content">
+            {titleCollapse.map((title, index) => (
+                <div key={index}>
+                    <div className="collapse">
+                        <p className="collapse__title">{title}</p>
+                        <img
+                            className={`collapse__chevron ${expandedSections.includes(index) ? 'collapse__chevron--anim' : ''}`}
+                            onClick={() => toggleSection(index)}
+                            src={chevronUp}
+                            alt="Chevron"
+                        />
+                    </div>
+                    {expandedSections.includes(index) && (
+                        <div>
+                            <p className="collapse__comment">{commentCollapse[index]}</p>
+                        </div>
+                    )}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default Collapse;
