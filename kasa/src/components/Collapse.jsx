2b2cd02c781783aab4107700a8b6a6cdf6f2a435
teddy8587 <teddy.barieraud@gmail.com>
2024-07-14 22:21:47 +0200

    feat animation d'apparition des commentaires

--- kasa/src/components/Collapse.jsx
+++ kasa/src/components/Collapse.jsx
@@ -3,7 +3,6 @@ import "../styles/Collapse.css";
 import chevronUp from "../assets/chevron-up-solid.svg";
 
 function Collapse() {
-
     const titleCollapse = [
         'Fiabilité',
         'Respect',
@@ -12,20 +11,23 @@ function Collapse() {
     ];
 
     const commentCollapse = [
-        'Les annonces postées sur Kasa garantissent une fiabilité totales. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
-        'La bienveillance fait partie des valeurs fondatrice de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
-        'La bienveillance fait partie des valeurs fondatrice de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+        'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
         'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'
     ];
 
     const [expandedSections, setExpandedSections] = useState([]);
+    const [reversedSections, setReversedSections] = useState([]);
 
     const toggleSection = (index) => {
         if (expandedSections.includes(index)) {
-            // Si l'index est déjà ouvert, le retire de la liste pour le fermer
+            setReversedSections([...reversedSections, index]);
             setExpandedSections(expandedSections.filter(secIndex => secIndex !== index));
+            setTimeout(() => {
+                setReversedSections(reversedSections.filter(secIndex => secIndex !== index));
+            }, 300); // La durée de l'animation (0.5s)
         } else {
-            // Sinon, l'ajoute à la liste pour l'ouvrir
             setExpandedSections([...expandedSections, index]);
         }
     };
@@ -43,10 +45,10 @@ function Collapse() {
                             alt="Chevron"
                         />
                     </div>
-                    {expandedSections.includes(index) && (
-                        <div>
-                            <p className="collapse__comment">{commentCollapse[index]}</p>
-                        </div>
+                    {(expandedSections.includes(index) || reversedSections.includes(index)) && (
+                        <p className={`collapse__comment ${expandedSections.includes(index) ? 'collapse__comment--anim' : ''} ${reversedSections.includes(index) ? 'collapse__comment--reverse' : ''}`}>
+                            {commentCollapse[index]}
+                        </p>
                     )}
                 </div>
             ))}
