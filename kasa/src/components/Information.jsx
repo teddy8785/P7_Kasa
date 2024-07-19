f0024498a190770b4a914048714e27258504c06f
teddy8587 <teddy.barieraud@gmail.com>
2024-07-19 22:15:34 +0200

    feat information page logement

--- kasa/src/components/Information.jsx
+++ kasa/src/components/Information.jsx
@@ -1,14 +1,43 @@
 import "../styles/Information.css";
-// import data from '../Data.json';
+import starRegular from "../assets/star-solid-grey.svg";
+import starSolid from "../assets/star-solid-red.svg"
+import { useParams } from 'react-router-dom';
+import data from '../Data.json';
 
 
 function Information() {
 
-    return (
-       <div className="information">
-        <h1>aaaa</h1>
-       </div>
+    const { id } = useParams();
+    const foundItem = data.find(item => item.id === id);
 
+    const { title, location, tags, host} = foundItem;
+    const { name: hostName, picture: hostPicture } = host;
+    
+    return (
+        <div className="information">
+            <div className="information__content">
+                <h1 className="information__title">{title}</h1>
+                <p className="information__location">{location}</p>
+                <div className="information__filters--content">
+                    {tags.map((tag, index) => (
+                        <p key={`${tag}-${index}`} className="information__filters">{tag}</p>
+                    ))}
+                </div>
+            </div>
+            <div className="information__portrait">
+            <div className="information__host">
+                <p className="information__name">{hostName}</p>
+                <img className="information__photo" src={hostPicture} alt={`portrait de ${hostName}`} />
+            </div>
+            <div className="information__rating">
+               
+            </div>
+
+            <div>
+                
+            </div>
+            </div>
+        </div>
     )
 }
 
